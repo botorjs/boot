6b1b5d0904242bbecf7e51e0f00448f5d6ae6510
norton <norton0395@gmail.com>
2019-03-06 13:45:30 +0700

    update

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { Boot } from './src/boot';
+import { TypeContainer } from './src/types';
+import { ServiceProvider } from './src/ServiceProvider';
+import { Inject } from './src/decorators/Inject';
+import { Injectable } from './src/decorators/Injectable';
+
+export { Boot,TypeContainer, ServiceProvider, Injectable, Inject };
